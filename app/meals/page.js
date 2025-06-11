da4967475ae24513a274be7bb7b9aae24869c45d
Isaac Koomson <iekay@Isaacs-MacBook-Pro.local>
2025-06-11 20:44:53 +0000

    update

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,20 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals-output/meals-grid';
 import {getMeals} from '@/lib/meals';
-export default async function MealsPage(){
-    const meals = await getMeals();
+import { Suspense } from 'react';
+
+
+async function Meals(){
+     const meals = await getMeals();
+
+     return <MealsGrid meals={meals}/>
+
+
+
+}
+
+
+export default  function MealsPage(){
     
     return (
         <>
@@ -21,7 +33,9 @@ export default async function MealsPage(){
             </header>
 
             <main className={classes.main}>
-                <MealsGrid meals={meals}/>
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals/>
+                </Suspense>
 
             </main>
         </>
